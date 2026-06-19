cce1a364ae397915f4a5886be3addaae8fcaf706
lilianaayala5824-lgtm <liliana.ayala5824@utc.edu.ec>
2026-06-19 00:24:26 -0500

    cambio 6 Lili

--- deber/src/CarrosModel/carros_model.java
+++ deber/src/CarrosModel/carros_model.java
@@ -9,5 +9,58 @@ package CarrosModel;
  * @author PC
  */
 public class carros_model {
-    
+  
+    private int codigo;
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    public carros_model(int codigo, String marca, String modelo, double precio) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo +
+               " | Marca: " + marca +
+               " | Modelo: " + modelo +
+               " | Precio: $" + precio;
+    }
 }
+    
+
