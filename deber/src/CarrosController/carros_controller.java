bd3a05e2f4585799b31a9f4535abe74192017511
lilianaayala5824-lgtm <liliana.ayala5824@utc.edu.ec>
2026-06-19 00:26:51 -0500

    cambio 7 Lliana

--- deber/src/CarrosController/carros_controller.java
+++ deber/src/CarrosController/carros_controller.java
@@ -3,11 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CarrosController;
-
+    import java.util.ArrayList;
+import CarrosModel.carros_model;
 /**
  *
  * @author PC
  */
 public class carros_controller {
+    
+    public ArrayList<carros_model> listaCarros = new ArrayList<>();
+
+    public void agregarCarro(carros_model carro) {
+        listaCarros.add(carro);
+        System.out.println("Carro agregado");
+    }
     
 }
