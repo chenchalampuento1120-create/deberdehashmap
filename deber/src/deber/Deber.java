16fa127df4375a584bf02efb0d8b314d25ca6ffd
AEINK8090 <alexis.caisaguano5790@utc.edu.ec>
2026-06-19 08:19:44 -0500

    Agregar mi menu_hashmap al menú principal

--- deber/src/deber/Deber.java
+++ deber/src/deber/Deber.java
@@ -10,6 +10,7 @@ package deber;
  */
 import CarrosView.carros_view;
 import ComprasView.ComprasView;
+import deber.menu_hashmap;
 import java.util.Scanner;
 
 /**
@@ -24,6 +25,7 @@ public class Deber{
 
         ComprasView compras = new ComprasView();
         carros_view carros = new carros_view();
+        menu_hashmap hashmap = new menu_hashmap();
 
         int opcion;
 
@@ -31,7 +33,8 @@ public class Deber{
             System.out.println("===== MENU PRINCIPAL =====");
             System.out.println("1. CRUD Compras");
             System.out.println("2. CRUD Carros");
-            System.out.println("3. Salir");
+            System.out.println("3. Menu Hashmap");
+            System.out.println("4. Salir");
             System.out.print("Seleccione una opcion: ");
 
             opcion = sc.nextInt();
@@ -46,6 +49,10 @@ public class Deber{
                     break;
 
                 case 3:
+                    menu_hashmap.main(null);
+                    break;
+                    
+                case 4:
                     System.out.println("Saliendo...");
                     break;
 
@@ -53,6 +60,6 @@ public class Deber{
                     System.out.println("Opcion incorrecta");
             }
 
-        } while (opcion != 3);
+        } while (opcion != 4);
     }
 }
