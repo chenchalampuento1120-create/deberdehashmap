71ce81070e796a966f3eec8cf36071f1c1a4a6ed
chenchalampuento1120-create <chen.chalampuento1120@utc.edu.ec>
2026-06-19 01:27:30 -0500

    cambios chen 13

--- deber/src/deber/Deber.java
+++ deber/src/deber/Deber.java
@@ -8,7 +8,9 @@ package deber;
  *
  * @author PC
  */
+import CarrosView.carros_view;
 import ComprasView.ComprasView;
+import java.util.Scanner;
 
 /**
  *
@@ -18,11 +20,39 @@ public class Deber{
 
     public static void main(String[] args) {
         
-        ComprasView vista = new ComprasView();
-        vista.menu();
-        
-        carros_view vista = new carros_view();
-        vista.menu();
+       Scanner sc = new Scanner(System.in);
 
+        ComprasView compras = new ComprasView();
+        carros_view carros = new carros_view();
+
+        int opcion;
+
+        do {
+            System.out.println("===== MENU PRINCIPAL =====");
+            System.out.println("1. CRUD Compras");
+            System.out.println("2. CRUD Carros");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opcion: ");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    compras.menu();
+                    break;
+
+                case 2:
+                    carros.menu();
+                    break;
+
+                case 3:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+
+        } while (opcion != 3);
     }
 }
