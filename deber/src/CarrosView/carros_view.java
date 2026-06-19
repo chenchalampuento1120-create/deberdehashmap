debd425f66c972f7d61e93253bbe4a4615e4829a
lilianaayala5824-lgtm <liliana.ayala5824@utc.edu.ec>
2026-06-19 00:53:53 -0500

    cambio 10 Liliana

--- deber/src/CarrosView/carros_view.java
+++ deber/src/CarrosView/carros_view.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CarrosView;
+    import java.util.Scanner;
+import CarrosController.carros_controller;
+import CarrosModel.carros_model;
 
 /**
  *
@@ -11,3 +14,21 @@ package CarrosView;
 public class carros_view {
     
 }
+    Scanner sc = new Scanner(System.in);
+    carros_controller controlador = new carros_controller();
+
+    public void menu() {
+        while (true) {
+
+            System.out.println("===== CONCESIONARIO =====");
+            System.out.println("1. Agregar carro");
+            System.out.println("2. Mostrar carros");
+            System.out.println("3. Actualizar carro");
+            System.out.println("4. Eliminar carro");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione: ");
+
+            int opcion = sc.nextInt();
+    }
+    }
+    
